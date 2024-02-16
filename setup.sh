84ce4800e658ace99d22dd12c17c918e72601442
Infamousmick <mmarchese6690@gmail.com>
2024-02-16 19:41:57 +0100

    fix2

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
 ################################################################################
-rm -rf "$(pwd)/InfaScripts"
+rm -rf "$(pwd)/infaScript"
 clone() {
     git clone https://github.com/Infamousmick/infaScript.git > /dev/null 2>&1
-    cd InfaScript
+    cd infaScript
     chmod +x -R bin/
-    chmod +x bin/InfaScript.sh
+    chmod +x InfaScript.sh
     chmod +x bin/AdbCommands.sh
     chmod +x bin/GmsDisabler.sh
     chmod +x bin/GmsEnabler.sh
