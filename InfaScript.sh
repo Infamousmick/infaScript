807d0c5d8de7d8dd198a3c45bdff113ed8f28ec0
Ravindu Deshan <droidcasts@protonmail.com>
2024-02-17 19:12:17 +0530

    Fixed : Read issues ?

--- InfaScript.sh
+++ InfaScript.sh
@@ -2,25 +2,26 @@
 
 # Function to prompt for exit confirmation
 exit_a() {
-    read -r -p "
-    Do you want to exit? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Do you want to exit? (1=YES, 2=NO)"
+    read -r -p "Enter your choice : " input
     case $input in
         1)
             echo "
             Press ENTER to exit"
-            read -r
+            read -r a
             exit 0
             ;;
         2)
             echo "
             Press ENTER to return to START"
-            read -r
+            read -r a
             start
             ;;
         *)
             echo "
             Choose a valid option, press ENTER to continue..."
-            read -r
+            read -r a
             exit_a
             ;;
     esac
@@ -28,8 +29,9 @@ exit_a() {
 
 # Function to run adb preset commands
 adbC() {
-    read -r -p "
-    Are you sure to run Adb Preset Commands? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Are you sure to run Adb Preset Commands? (1=YES, 2=NO)"
+    read -r -p "Enter your choice : " input
     case $input in
         1)
             ./bin/AdbCommands.sh
@@ -40,7 +42,7 @@ adbC() {
         *) 
             echo "
             Choose a valid option, press ENTER to continue..."
-            read -r
+            read -r a
             adbC
             ;;
     esac
@@ -48,17 +50,18 @@ adbC() {
 
 # Function for GMS operations
 gms_a() {
-    read -r -p "
+    echo "
     What do you want to run?
     1. GMS Disabler
     2. GMS Enabler
     3. Start
-    4. Exit
-    Enter your choice: " input
+    4. Exit"
+    read -r -p "Enter your choice : " input
     case $input in
         1)
-            read -r -p "
-            Are you sure to run GMS Disabler? (1=YES, 2=NO) Enter your choice: " answ
+            echo "
+            Are you sure to run GMS Disabler? (1=YES, 2=NO)"
+            read -r answ
             if [ "$answ" -eq 1 ]; then
                 ./bin/GmsDisabler.sh
             elif [ "$answ" -eq 2 ]; then
@@ -66,13 +69,14 @@ gms_a() {
             else
                 echo "
                 Choose a valid option, press ENTER to continue..."
-                read -r
+                read -r a
                 gms_a
             fi
             ;;
         2)
-            read -r -p "
-            Are you sure to run GMS Enabler? (1=YES, 2=NO) Enter your choice: " answ
+            echo "
+            Are you sure to run GMS Enabler? (1=YES, 2=NO)"
+            read -r answ
             if [ "$answ" -eq 1 ]; then
                 ./bin/GmsEnabler.sh
             elif [ "$answ" -eq 2 ]; then
@@ -80,7 +84,7 @@ gms_a() {
             else
                 echo "
                 Choose a valid option, press ENTER to continue..."
-                read -r
+                read -r a
                 gms_a
             fi
             ;;
@@ -97,8 +101,9 @@ gms_a() {
 
 # Function to boost performance
 boostp() {
-    read -r -p "
-    Are you sure to run Boost Performance? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Are you sure to run Boost Performance? (1=YES, 2=NO)"
+    read -r -p "Enter your choice : " input
     case $input in
         1)
             clear
@@ -106,12 +111,12 @@ boostp() {
             ####################### Boost Performance ######################
             Run this command once a week...
             Running Performance optimizations command..."
-            read -r
+            read -r a
             cmd package compile -m speed-profile -a
             echo "
             #######################      FINISH      #######################
             Press ENTER to continue..."
-            read -r
+            read -r a
             start
             ;;
         2)
@@ -120,7 +125,7 @@ boostp() {
         *)
             echo "
             Choose a valid option, press ENTER to continue..."
-            read -r
+            read -r a
             boostp
             ;;
     esac
@@ -128,77 +133,81 @@ boostp() {
 
 # Function to boost battery
 boostb() { 
-    read -r -p "
-    Are you sure to run Boost Battery? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Are you sure to run Boost Battery? (1=YES , 2=NO)
+    "
+    read -r -p "Enter your choice : " input
     case $input in
-        1)  
-            clear
-            echo "
-            ####################### Boost Battery ######################
-            To run this command, keep your phone plugged in and be sure it is at 100%, otherwise the script will FAIL!!!!
-            Run this command once a month...
-            Running Battery Boost command..."
-            read -r
-            cmd package bg-dexopt-job
-            echo "
-            #######################     FINISH    ######################
-            Press ENTER to continue..."
-            read -r
-            start
-            ;;
-        2)
-            start
-            ;;
-        *)
-            echo "
-            Choose a valid option, press ENTER to continue..."
-            read -r
-            boostb
-            ;;
+    1)  
+    clear
+    echo "
+    ####################### Boost Battery ######################
+    To run this command plug keep your phone in charging and be sure it is at 100% otherwise the script will FAIL!!!!
+    Run this commands once a month...
+    Running Battery Boost command..."
+    read -r a
+    cmd package bg-dexopt-job
+    echo "
+    #######################     FINISH    ######################
+    Press ENTER to continue..."
+    read -r a
+    start
+    ;;
+    2)
+    start
+    ;;
+    *)
+    echo "
+    Choose a valid option, press ENTER to continue..."
+    read -r a
+    boostb
+    ;;
     esac
+
 }
 
-# Function to clear cache
 cache() {
-    read -r -p "
-    Are you sure to run Clear Cache? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Are you sure to run Clear Cache? (1=YES , 2=NO)
+    "
+    read -r -p "Enter your choice : " input
     case $input in
-        1)
-            ./bin/Cache.sh
-            ;;
-        2)
-            start
-            ;;
-        *)    
-            echo "
-            Choose a valid option, press ENTER to continue..."
-            read -r
-            cache
-            ;;
+    1)
+    ./bin/Cache.sh
+    ;;
+    2)
+    start
+    ;;
+    *)    
+    echo "
+    Choose a valid option, press ENTER to continue..."
+    read -r a
+    cache
+    ;;
     esac
 }
 
-# Function to reboot
 reboot() {
-    read -r -p "
-    Are you sure to run Reboot Menu? (1=YES, 2=NO) Enter your choice: " input
+    echo "
+    Are you sure to run Reboot Menu? (1=YES , 2=NO)
+    "
+    read -r -p "Enter your choice : " input
     case $input in
-        1)
-            ./bin/RebootMenu.sh
-            ;;
-        2)
-            start
-            ;;
-        *)
-            echo "
-            Choose a valid option, press ENTER to continue..."
-            read -r
-            reboot
-            ;;
+    1)
+    ./bin/RebootMenu.sh
+    ;;
+    2)
+    start
+    ;;
+    *)
+    echo "
+    Choose a valid option, press ENTER to continue..."
+    read -r a
+    reboot
     esac
+   
 }
 
-# Function to display main menu
 start() {
     clear
     echo "
@@ -216,7 +225,6 @@ start() {
     read -r answer
 }
 
-# Main loop for menu navigation
 while true; do
     start
     case $answer in
@@ -244,7 +252,7 @@ while true; do
         *)   
             echo "
             Choose a valid option, press ENTER to continue..."
-            read -r
+            read -r a
             start
             ;;
     esac
