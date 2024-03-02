97c33d26f72fe075f95c9f2aef28065acc4b7882
Infamousmick <mmarchese6690@gmail.com>
2024-03-02 17:52:01 +0100

    f

--- bin/AdbCommands.sh
+++ bin/AdbCommands.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-adbC() {
-    clear
-    echo -e "
-    ${RESET}${GREEN}${txtbggry}${BOLD}Are you sure to run Adb Preset Commands? (1=YES, 2=NO)
-    ${BLUE}${txtbggry}${BOLD}Enter your choice: "
-    read -r choice
-    case $choice in
-        1)
-            start
-            ;;
-        2)
-            exit 10
-            ;;
-        *) 
-            echo -e "
-            ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
-            read -r a
-            adbC
-            ;;
-    esac
-}
-adbC
 
 start() {
     clear
@@ -631,3 +609,26 @@ while true; do
             ;;
     esac
 done
+
+adbC() {
+    clear
+    echo -e "
+    ${RESET}${GREEN}${txtbggry}${BOLD}Are you sure to run Adb Preset Commands? (1=YES, 2=NO)
+    ${BLUE}${txtbggry}${BOLD}Enter your choice: "
+    read -r choice
+    case $choice in
+        1)
+            start
+            ;;
+        2)
+            exit 10
+            ;;
+        *) 
+            echo -e "
+            ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
+            read -r a
+            adbC
+            ;;
+    esac
+}
+adbC
