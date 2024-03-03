97807a9b7e005cd3cdcae6d0c43ce3101852c3b2
Infamousmick <mmarchese6690@gmail.com>
2024-03-03 14:34:09 +0100

    fix

--- bin/AdbBackup.sh
+++ bin/AdbBackup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 adb_backup() {
-clear
-backup_folder="ADB_Backup"
+    clear
+    backup_folder="ADB_Backup"
 
 # Verificare se la cartella "ADB Backup" esiste, altrimenti crearla
 if [ ! -d "$backup_folder" ]; then
@@ -15,33 +15,33 @@ if [ ! -d "$backup_folder" ]; then
 fi
 
 # Eseguire il comando "adb shell settings list" per i parametri "system", "secure" e "global"
-system_settings=$(settings list system)
-secure_settings=$(settings list secure)
-global_settings=$(settings list global)
+    system_settings=$(settings list system)
+    secure_settings=$(settings list secure)
+    global_settings=$(settings list global)
 
 # Chiedere all'utente il nome dei file in uscita
-echo -e "Choose a prefix file name: "
-read -r  filename_prefix
-echo "Add the date to add to the file: "
-read -r  file_date
+    echo -e "Choose a prefix file name: "
+    read -r  filename_prefix
+    echo "Add the date to add to the file: "
+    read -r  file_date
 
 # Salvare i file nella cartella "ADB Backup" con i nomi e le date specificati
-system_filename="$backup_folder/${filename_prefix}_system_$file_date.txt"
-secure_filename="$backup_folder/${filename_prefix}_secure_$file_date.txt"
-global_filename="$backup_folder/${filename_prefix}_global_$file_date.txt"
+    system_filename="$backup_folder/${filename_prefix}_system_$file_date.txt"
+    secure_filename="$backup_folder/${filename_prefix}_secure_$file_date.txt"
+    global_filename="$backup_folder/${filename_prefix}_global_$file_date.txt"
 
-echo "$system_settings" > "$system_filename"
-echo "$secure_settings" > "$secure_filename"
-echo "$global_settings" > "$global_filename"
+    echo "$system_settings" > "$system_filename"
+    echo "$secure_settings" > "$secure_filename"
+    echo "$global_settings" > "$global_filename"
 
-echo -e "File saved successfully into '$backup_folder':"
-echo -e "${txtbggrn}${bold}- $system_filename"
-echo -e "${txtbggrn}${bold}- $secure_filename"
-echo -e "${txtbggrn}${bold}- $global_filename${RESET}"
-chmod u+x -R ADB_Backup/* ; chmod u+x *
-echo -e "
-    ${RESET}${txtinv}${BOLD}Press ENTER to return to ADB Commands Start...${RESET}"
-read -r a
-exit 0
+    echo -e "File saved successfully into '$backup_folder':"
+    echo -e "${txtbggrn}${bold}- $system_filename"
+    echo -e "${txtbggrn}${bold}- $secure_filename"
+    echo -e "${txtbggrn}${bold}- $global_filename${RESET}"
+    chmod u+x -R ADB_Backup/* ; chmod u+x *
+    echo -e "
+        ${RESET}${txtinv}${BOLD}Press ENTER to return to ADB Commands Start...${RESET}"
+    read -r a
+    exit 0
 }
 adb_backup
--- bin/AdbCommands.sh
+++ bin/AdbCommands.sh
@@ -2,35 +2,38 @@
 
 
 start() {
-clear
-echo -e "
-${RESET}${txtbgrst}${BLUE}${BOLD}########## ADB MENU ##########${WHITE}${BOLD}
-Choose what to do?
-1.  Run adb Preset inside the script
-2.  Reset the value of the Preset adb
-3.  ADB Preset Backup
-${RED}${BOLD}4.  Return to InfaScript Start
-${RESET}${txtbgrst}${BLUE}${BOLD}##############################${txtbgrst}${BLUE}${BOLD}
-Enter your choice: "
+    clear
+    echo -e "
+    ${RESET}${txtbgrst}${BLUE}${BOLD}########## ADB MENU ##########${WHITE}${BOLD}
+    Choose what to do?
+    1.  Run adb Preset inside the script
+    2.  Reset the value of the Preset adb
+    3.  ADB Preset Backup
+    ${RED}${BOLD}4.  Return to InfaScript Start
+    ${RESET}${txtbgrst}${BLUE}${BOLD}##############################${txtbgrst}${BLUE}${BOLD}
+    Enter your choice: "
     read -r choice
     case $choice in
         1)
-        sh bin/AdbRun.sh
-        ;;
-    2) 
-        sh bin/Adbreset.sh
-        ;;
-    3)
-        sh bin/AdbBackup.sh
-        ;;
-    4)
-        echo -e "
-        ${RESET}${txtinv}${BOLD}Press ENTER to return to Start${RESET}"
-        read -r a
-        exit 0
-        ;;
-    *)  
-    ;; 
+            sh bin/AdbRun.sh
+            ;;
+        2) 
+            sh bin/Adbreset.sh
+            ;;
+        3)
+            sh bin/AdbBackup.sh
+            ;;
+        4)
+            echo -e "
+            ${RESET}${txtinv}${BOLD}Press ENTER to return to Start${RESET}"
+            read -r a
+            exit 0
+            ;;
+        *)  
+            echo -e "${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
+            read -r a
+            start
+            ;; 
     esac
 }
 
