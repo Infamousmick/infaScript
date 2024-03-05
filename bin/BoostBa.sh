2adee29d377f17fd05101d17fe40879d5601453e
Infamousmick <mmarchese6690@gmail.com>
2024-03-05 10:21:12 +0100

    fix

--- bin/BoostBa.sh
+++ bin/BoostBa.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 boostb() {
     clear
-    printf "    ${RESET}${GREEN}${BOLD}Are you sure to run Boost Battery? (1=YES, 2=NO)${BLUE}${BOLD}
-    Enter your choice: "
-    read -r input
-    case $input in
+    printf "    
+        ${RESET}${GREEN}${BOLD}Are you sure to run Boost Battery? (1=YES, 2=NO)${BLUE}${BOLD}
+        Enter your choice: "
+        read -r input
+        case $input in
         1)
             clear
-            printf "            ${RESET}${txtbgrst}${BLUE}${BOLD}########## Boost Battery ##########${WHITE}${BOLD}
+            printf "
+            {RESET}${txtbgrst}${BLUE}${BOLD}########## Boost Battery ##########${WHITE}${BOLD}
             To run this command plug keep your 
             phone in charging and be sure it is 
             at 100%, otherwise the script will 
@@ -27,7 +29,8 @@ boostb() {
             exit 10
             ;;
         *)
-            printf "    ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
+            printf "
+            ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
             read -r a
             boostb
             ;;
