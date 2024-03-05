901d51c4197792d2794f3603399dffc97664fa68
Infamousmick <113915713+Infamousmick@users.noreply.github.com>
2024-03-05 10:08:11 +0100

    update bin/BoostBa.sh

--- bin/BoostBa.sh
+++ bin/BoostBa.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 boostb() {
     clear
-    printf "${RESET}${GREEN}${BOLD}Are you sure to run Boost Battery? (1=YES, 2=NO)${BLUE}${BOLD}
+    printf "    ${RESET}${GREEN}${BOLD}Are you sure to run Boost Battery? (1=YES, 2=NO)${BLUE}${BOLD}
     Enter your choice: "
     read -r input
     case $input in
         1)
             clear
-            printf "${RESET}${txtbgrst}${BLUE}${BOLD}########## Boost Battery ##########${WHITE}${BOLD}
+            printf "    ${RESET}${txtbgrst}${BLUE}${BOLD}########## Boost Battery ##########${WHITE}${BOLD}
             To run this command plug keep your 
             phone in charging and be sure it is 
             at 100%, otherwise the script will 
@@ -18,7 +18,7 @@ boostb() {
             sleep 2
             cmd package bg-dexopt-job
             clear
-            printf "${txtbgrst}${BLUE}${BOLD}############# FINISH ##############${RESET}${BLUE}${BOLD}
+            printf "    ${txtbgrst}${BLUE}${BOLD}############# FINISH ##############${RESET}${BLUE}${BOLD}
             ${RESET}${txtinv}${BOLD}Press ENTER to continue..."
             read -r a
             exit 10
@@ -27,8 +27,7 @@ boostb() {
             exit 10
             ;;
         *)
-            printf "
-            ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
+            printf "    ${RESET}${txtinv}${BOLD}Choose a valid option, press ENTER to continue...${RESET}"
             read -r a
             boostb
             ;;
