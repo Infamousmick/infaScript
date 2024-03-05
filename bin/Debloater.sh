b97ea7e2c7048114c12b7a1f38c1beae5802b00a
Infamousmick <mmarchese6690@gmail.com>
2024-03-05 12:10:14 +0100

    test

--- bin/Debloater.sh
+++ bin/Debloater.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-debloater() {
+debloateren() {
+    while IFS= read -r app || [ -n "$app" ]; do
+    if [ -n "$app" ]; then
+        pm uninstall -k --user 0 "$app"
+        if [ $? -eq 0 ]; then
+            printf "App $app disabled successfully."
+        else
+            printf "Error disabling app $app."
+        fi
+    fi
+    
+done < "debloat_list.txt"
+printf "
+${RESET}${RED}${txtbgblu}${BOLD}Enabled list apps uninstalled 
+${RESET}${txtinv}${BOLD}press ENTER to return back..${RESET}"
+read -r a
+sh bin/Appsrun.sh
+}
+debloaterdeb() {
 while IFS= read -r app || [ -n "$app" ]; do
     if [ -n "$app" ]; then
         pm uninstall -k --user 0 "$app"
@@ -17,7 +35,43 @@ ${RESET}${txtinv}${BOLD}press ENTER to return back..${RESET}"
 read -r a
 sh bin/Appsrun.sh
 }
-debloater
+
+
+start(){
+printf "
+    ${RESET}${GREEN}${BOLD}Choose an option: ${WHITE}${BOLD}
+    1.  Enable from Enabled list
+    2.  Enable from Disabled list
+    ${MAGENTA}${BOLD}3.  Return back
+    ${RED}${BOLD}4.  Exit
+    ${BLUE}${BOLD}"
+    read -r choice
+
+    case $choise in
+        1)
+            debloateren
+            ;;
+        2)
+            debloaterdeb
+            ;;
+        3)
+            sh bin/Appsrun.sh
+            ;;
+        4)
+            printf "
+            ${RESET}${RED}${BOLD}Press ENTER to exit"
+            read -r a
+            clear
+            pkill -f InfaScript.sh
+            ;;
+        *)
+            printf "${RESET}${txtinv}${BOLD}Invalid choice. Press ENTER to continue...${RESET}"
+            read -r a
+            remove_apps
+            ;;
+    esac
+}
+start
 
 
 
--- bin/Enabler.sh
+++ bin/Enabler.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-enabler() {
+enableen()  {
+while IFS= read -r app || [ -n "$app" ]; do
+    if [ -n "$app" ]; then
+        cmd package install-existing "$app"
+        if [ $? -eq 0 ]; then
+            printf "App $app disabled successfully."
+        else
+            printf "Error disabling app $app."
+        fi
+    fi
+done < "enable_list.txt"
+printf "
+${RESET}${RED}${txtbgblu}${BOLD}Enabled list apps reinstalled 
+${RESET}${txtinv}${BOLD}press ENTER to return back..${RESET}"
+read -r a
+sh bin/Appsrun.sh
+}
+enabledeb() {
 while IFS= read -r app || [ -n "$app" ]; do
     if [ -n "$app" ]; then
         cmd package install-existing "$app"
@@ -16,4 +33,39 @@ ${RESET}${txtinv}${BOLD}press ENTER to return back..${RESET}"
 read -r a
 sh bin/Appsrun.sh
 }
-enabler
+
+start(){
+printf "
+    ${RESET}${GREEN}${BOLD}Choose an option: ${WHITE}${BOLD}
+    1.  Enable from Enabled list
+    2.  Enable from Disabled list
+    ${MAGENTA}${BOLD}3.  Return back
+    ${RED}${BOLD}4.  Exit
+    ${BLUE}${BOLD}"
+    read -r choice
+
+    case $choise in
+        1)
+            enableen
+            ;;
+        2)
+            enabledeb
+            ;;
+        3)
+            sh bin/Appsrun.sh
+            ;;
+        4)
+            printf "
+            ${RESET}${RED}${BOLD}Press ENTER to exit"
+            read -r a
+            clear
+            pkill -f InfaScript.sh
+            ;;
+        *)
+            printf "${RESET}${txtinv}${BOLD}Invalid choice. Press ENTER to continue...${RESET}"
+            read -r a
+            remove_apps
+            ;;
+    esac
+}
+start
