5e766082fd0e809143944119849985fceafeaa81
@ravindu644 <droidcasts@protonmail.com>
2024-03-06 20:33:51 +0530

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -11,7 +11,9 @@ DIR=$(pwd)
 
 setup(){
     rm -rf "$DIR/infaScript" > /dev/null 2>&1
-    echo -e "\nInstalling Requirements...\n" ; apt install git tsu -y ; echo -e "\n[i] Done..!\n"
+    echo -e "\nInstalling Requirements...\n"
+    pkg install git tsu -y
+    echo -e "\n[i] Done..!\n"
     git clone https://github.com/Infamousmick/infaScript.git -b stable_up ;cd $DIR/infaScript
     chmod +x -R *
 }
