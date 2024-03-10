a200c35ddc2a719ade2cb71a86fbb357b213157f
@ravindu644 <droidcasts@protonmail.com>
2024-03-10 08:58:38 +0530

    Fixes(2)

--- setup.sh
+++ setup.sh
@@ -13,10 +13,10 @@ setup(){
     rm -rf "$DIR/infaScript" > /dev/null 2>&1
     echo -e "\nCloning Script...\n"
     git clone https://github.com/Infamousmick/infaScript.git -b stable_up
-    echo -e "\n[i] Done.\n"
-    echo -e "\n[i] Run "infa" to start..!\n"
     cd $DIR/infaScript
     chmod +x -R *
     rm /data/data/com.termux/files/usr/bin/infa > /dev/null 2>&1 ; ln -s /data/data/com.termux/files/home/infaScript/infa /data/data/com.termux/files//usr/bin/infa
+    echo -e "\n[i] Done.\n"
+    echo -e "\n[i] Run "infa" to start..!\n"    
 }
 setup
