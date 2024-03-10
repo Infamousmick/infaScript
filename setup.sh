60107fe2f10045b11358f0f4e74a4f0d858cbdfc
@ravindu644 <droidcasts@protonmail.com>
2024-03-10 09:16:55 +0530

    Fix : colors

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
 ################################################################################
-cd $HOME
+cd "$HOME"
 DIR=$(pwd)
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;\033m'
+YELLOW='\033[0;33m'
 BOLD='\033[1m'
 RESET='\033[0m'
 
@@ -21,10 +21,10 @@ setup(){
     pkg install wget git tsu -y
     echo -e "${GREEN}\n[+] Cloning Script...\n${RESET}"
     git clone https://github.com/Infamousmick/infaScript.git -b stable_up
-    cd $DIR/infaScript
+    cd "$DIR/infaScript"
     chmod +x -R *
     rm /data/data/com.termux/files/usr/bin/infa > /dev/null 2>&1 ; ln -s /data/data/com.termux/files/home/infaScript/infa /data/data/com.termux/files//usr/bin/infa
-    echo -e "\n${YELLOW}[i] Done.\n${RESET}"
-    echo -e "${BOLD}${GREEN}\n[i] Run 'infa' to get started..!\n${RESET}"    
+    echo -e "\n${YELLOW}[i] Done.${RESET}\n"
+    echo -e "${BOLD}${GREEN}[i] Run 'infa' to get started..!${RESET}\n"    
 }
 setup
