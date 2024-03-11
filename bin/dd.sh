a1e8275e9d8de325f6efb07d56dd8018b8fff35c
Infamousmick <113915713+Infamousmick@users.noreply.github.com>
2024-03-11 20:05:44 +0100

    Added partitions in dd.sh

--- bin/dd.sh
+++ bin/dd.sh
@@ -25,7 +25,7 @@ fi
 
 # Lista di possibili percorsi delle partizioni Android
 
-possible_partitions=("/dev/block/by-name/" "/dev/block/sdcard" "/dev/block/by-num/" "/dev/block/bootdevice/by-name/")
+possible_partitions=("/dev/block/by-name/" "/dev/block/sdcard" "/dev/block/by-num/" "/dev/block/bootdevice/by-name/" "/dev/block/bootdevice/sdcard" "/dev/block/bootdevice/by-num/")
 
 
 
