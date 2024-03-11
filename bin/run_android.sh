89341ca0630c866c71a98793e6ed5e14f0a4606a
Infamousmick <mmarchese6690@gmail.com>
2024-03-11 15:49:16 +0100

    added marshmallow land

--- bin/run_android.sh
+++ bin/run_android.sh
@@ -8,6 +8,7 @@ export BLUE='\033[0;34m'
 export RESET='\033[0m'
 export BOLD_GREEN='\033[1;32m'
 export BOLD_WHITE='\033[1;37m'
+
 batteryopt() {
     #Battery Optimizations Settings
     printf "\n${RESET}   ${BLUE}${BOLD}########## Battery Optimizations Settings ##########${RESET}\n\n"
@@ -29,8 +30,30 @@ batteryopt() {
     printf "\n%.0s" {1..100}
     clear
     start
-
 }
+marshmallowland() {
+    #Marshmallow Land
+    printf "\n${RESET}   ${BLUE}${BOLD}########## Marshmallow Land ##########${RESET}\n\n"
+
+    if su -c 'true' >/dev/null 2>&1; then
+        sleep 1
+        printf "   ${YELLOW}[i] This will redirect you to the 'Marshmallow Land' .${RESET}\n"
+        sleep 1
+        printf "   ${YELLOW}[i] Press ENTER when you are ready${RESET}\n\n"
+        read -r a
+        su -c am start -n com.android.systemui/com.android.systemui.egg.MLandActivity
+        printf "   ${BOLD_GREEN}\n\n[+] Execution Succeed..! \n\n${RESET}"
+    else
+        printf "   ${RED}[i] You are not rooted, cannot run this tweaks.${RESET}\n"
+        sleep 1
+    fi
+    printf "${RED}Press \"Enter\" to return to the 'Android Tweaks' menu again${RESET}"
+    read -r a
+    printf "\n%.0s" {1..100}
+    clear
+    start
+}
+
 
 start() {
     printf "\n%.0s" {1..100}
@@ -39,6 +62,7 @@ start() {
 
     printf "   ${BOLD_WHITE}Choose what to do?\n\n${RESET}"
     printf "   1. Open Battery Optimizations Settings\n"
+    printf "   2. Marshmallow Land\n"
     printf "${RED}   2. Exit\n\n"
 
     printf "   ${RESET}${BLUE}${BOLD}########################################${RESET}\n"
@@ -48,7 +72,8 @@ start() {
 
     case $choice in
     1) batteryopt ;;
-    2)
+    2) marshmallowland ;;
+    3)
         printf "\n   ${RESET}${UNDERLINE}${BOLD}Press ENTER to return to Start${RESET}"
         read -r a
         exit 0
